41db94e6c518c77fe452ffa4d0ff031dfaf26d7b
igor21211 <spuraigor@gmail.com>
2023-11-23 00:32:21 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 const API_END_POINT_POSTS = "https://jsonplaceholder.typicode.com/posts/";
 const input = document.querySelector(".id__input");
+const commentsDiv = document.querySelector(".comments__container");
 const btn = document.querySelector(".btn__submit");
 const root = document.querySelector(".root");
 
-function getDataById(id, url) {
-  fetch(API_END_POINT + id);
-  then((response) => response.json())
+function getPostById(id, url) {
+  fetch(url + id)
+    .then((response) => response.json())
     .then((data) => renderPost(data))
+    .catch((error) => {
+      throw error;
+    });
+}
+
+function getCommentByPostId(id, url) {
+  fetch(`${url}${id}/comments`)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((d) => renderComment(d));
+    })
     .catch((error) => {
       throw error;
     });
@@ -28,14 +40,16 @@ function renderPost(data) {
     `;
   divElement.append(btnShowComment);
   root.append(divElement);
-  btnShowComment.addEventListener("click", function () {});
+  btnShowComment.addEventListener("click", function () {
+    getCommentByPostId(data.id, API_END_POINT_POSTS);
+  });
 }
 
 btn.addEventListener("click", function () {
   clear();
   const id = input.value;
-  if (id < 100 && id >= 1) {
-    getDataById(id, API_END_POINT_POSTS);
+  if (id <= 100 && id >= 1) {
+    getPostById(id, API_END_POINT_POSTS);
   } else {
     showErrorInfo();
   }
@@ -47,4 +61,17 @@ function showErrorInfo() {
   pElement.style.color = "red";
   pElement.style.textAlign = "center";
   root.append(pElement);
+}
+
+function renderComment(data) {
+  const divElement = document.createElement("div");
+  divElement.classList.add("main__container");
+  divElement.innerHTML = `
+  <h2>Comments</h2>
+   <hr>
+      <p> Name : ${data.name}</p>
+      <p> Email : ${data.email}</p>
+      <p>${data.body}</p>
+  `;
+  root.appendChild(divElement);
 }
